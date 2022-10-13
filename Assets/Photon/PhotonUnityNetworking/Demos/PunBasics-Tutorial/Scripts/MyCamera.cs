59f844826a168b1209f82a6b2cd21257a77d4564
Patricio Dutra <patriciojdutra@gmail.com>
2022-10-13 03:12:36 -0300

    First commit

---
+++ Assets/Photon/PhotonUnityNetworking/Demos/PunBasics-Tutorial/Scripts/MyCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MyCamera : MonoBehaviourPunCallbacks
+{
+    
+    private Camera camera;
+    private AudioListener audioSource;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        audioSource = GetComponent<AudioListener>();
+
+        camera.gameObject.SetActive(photonView.IsMine);
+        audioSource.gameObject.SetActive(photonView.IsMine);
+    }
+}
